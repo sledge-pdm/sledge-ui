47f953c15a9458180ae738d141f153519221e3eb
Innsbluck <innsbluck@gmail.com>
2025-08-07 16:02:23 +0900

    considering (should quit using ve?

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,6 +1,5 @@
-import { assignInlineVars } from '@vanilla-extract/dynamic';
 import { type Component, type JSX, splitProps } from 'solid-js';
-import { fillVar, hoverFillVar, icon, pxVar, urlVar } from '../styles/icon.css';
+import '../styles/icon.css';
 
 interface IconProps extends JSX.HTMLAttributes<HTMLDivElement> {
   /** 透明背景 + 白(255,255,255) で描いた αマスク PNG */
@@ -25,20 +24,18 @@ const Icon: Component<IconProps> = (props) => {
   return (
     <div
       {...rest}
-      class={icon}
+      class="icon"
       style={{
+        '--icon-size': `${px}px`,
+        '--icon-fill': local.color ?? 'currentColor',
+        '--icon-hover-fill': local.hoverColor ?? local.color ?? 'currentColor',
+        '--icon-url': `url("${local.src}")`,
         transform: local.transform,
         '-webkit-transform': local.transform,
         filter: local.filter,
         '-webkit-filter': local.filter,
         'backdrop-filter': local.backdropFilter,
         '-webkit-backdrop-filter': local.backdropFilter,
-        ...assignInlineVars({
-          [pxVar]: `${px}px`, // ユニット付き
-          [fillVar]: local.color ?? 'currentColor',
-          [hoverFillVar]: local.hoverColor ?? local.color ?? 'currentColor',
-          [urlVar]: `url("${local.src}")`,
-        }),
       }}
     />
   );
