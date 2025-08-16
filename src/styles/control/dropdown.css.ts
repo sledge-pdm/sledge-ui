9de4700a953f242d46fc3c2d9f308ea62561e150
Innsbluck <innsbluck@gmail.com>
2025-08-16 19:03:55 +0900

    add canvas presets + improve preview performance

--- src/styles/control/dropdown.css.ts
+++ src/styles/control/dropdown.css.ts
@@ -59,6 +59,13 @@ export const menuStyle = style([
     minWidth: '100px',
     overflowY: 'auto',
     width: 'fit-content',
+
+    '::-webkit-scrollbar': {
+      width: '1px',
+    },
+    '::-webkit-scrollbar-thumb': {
+      backgroundColor: '#ddd',
+    },
   },
 ]);
 
